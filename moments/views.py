f53a36eb8437a0f14b3777ef2eea53b855a283ba
chenyf <chen_yifei2023@outlook.com>
2026-04-22 15:18:57 +0800

    3

--- moments/views.py
+++ moments/views.py
@@ -1,6 +1,8 @@
 from .models import WeChatUser, Status
 from django.shortcuts import redirect, render
 
+from config import APP_CODE
+from settings import ENVIRONMENT
 
 def home(request):
     return render(request, 'homepage.html')
@@ -19,11 +21,15 @@ def show_status(request):
     return render(request, 'status.html', {'statuses': statuses})
 
 
+
 def submit_post(request):
     user = WeChatUser.objects.get(user=request.user)
     text = request.POST.get('text')
     if text:
         status = Status(user=user, text=text)
         status.save()
-        return redirect('/status')
+        if ENVIRONMENT == 'dev':
+            return redirect(f'/status')
+        elif ENVIRONMENT == 'stag':
+            return redirect(f'/stag--{APP_CODE}/status')
     return render(request, 'my_post.html')
